d6b7ff771d5c8afdde112eca428a5820b5801bfc
lpurdy01 <lpurdy01@gmail.com>
2019-09-08 22:02:30 -0800

    Main Commit
    "Game" works, but is very difficult to move purposfully.
    
    Exe at Bin/CS484_HW.exe

---
+++ CS484_HW/Assets/Camera_Rotator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Rotator : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 baseOffset;
+    public float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float rotXZ = Time.deltaTime * moveHorizontal * rotSpeed;
+
+        //transform.RotateAround(player.transform.position, Vector3.up, rotXZ);
+
+    }
+
+    void LateUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Grab the horizontal user input
+
+        float rotY = Time.deltaTime * moveHorizontal * rotSpeed;  // Make sure the speed coresponds to the graphics refresh rate
+
+        Quaternion rotation = Quaternion.Euler(0.0f, rotY, 0.0f); // Create a Rotation Quaternion
+
+        Matrix4x4 m = Matrix4x4.Rotate(rotation);  // Create the rotation matrix 
+
+        baseOffset = m.MultiplyPoint3x4(baseOffset); // Rotate the base offset vector by multiplying it with the rotation matrix
+
+        transform.position = player.transform.position + baseOffset; // Add the new rotated offset vector to the player position
+
+        transform.LookAt(player.transform.position); // Position rotates, but I will use LookAt to simplify the final view.
+    }
+}
---
+++ CS484_HW/Assets/Player_Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    public float speed;
+    public float bounceMultiplier;
+    public float rotSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f , Mathf.Abs(moveVertical) * bounceMultiplier, moveVertical);
+        
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        GetComponent<Rigidbody>().AddRelativeTorque(transform.up * rotSpeed * Time.deltaTime * moveHorizontal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
